faedd3dce438d765d3c8b98fa6f20e5a8910fc25
webster13 <webster13@163.com>
2016-03-22 23:21:55 +0800

    基本完成了登录界面

--- doc/db/sys_user.sql
+++ doc/db/sys_user.sql
@@ -1,3 +1,5 @@
 truncate table sys_user;
+
+
 INSERT INTO `sys_user` VALUES (1, 'webster', 'da237dd6f76cd5829e174bd2e4dabd11', '5eb0a1d2a40738c1a5cac0e6381e2638', 'STAFF', 1, '2016-3-16 15:32:00', 1, '2016-3-16 15:32:04', 0);
 INSERT INTO `sys_user` VALUES (2, 'liuyun', 'a7190a81eee09b94cbcfc85e7e7bb824', '9c10e1089e255c4e136812cadcda8a17', 'STAFF', 1, '2016-3-16 15:32:00', 1, '2016-3-16 15:32:04', 0);
